17c5e35d4ccb6d4f4c630db0c2a5c3d44a673fcc
Pingbo Wen <pingbo.wen@tcl.com>
2022-12-01 17:44:48 +0800

    fix workflow errors
    Signed-off-by: Pingbo Wen <pingbo.wen@tcl.com>

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-# install dependency
+echo "install dependencies"
 sudo apt update
 sudo apt install -y make gcc g++ git bc
 
-# download toolchain
+echo "download toolchain"
 mkdir -pv ${GITHUB_WORKSPACE}/toolchain
 curl -L https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz -o ${GITHUB_WORKSPACE}/toolchain/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
 
 cd ${GITHUB_WORKSPACE}/toolchain
 tar xf arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
-export PATH=${GITHUB_WORKSPACE}/toolchain/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin:$PATH
+echo "${GITHUB_WORKSPACE}/toolchain/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin" >> ${GITHUB_PATH}
 aarch64-none-linux-gnu-gcc -v
 
